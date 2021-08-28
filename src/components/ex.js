f2df7cddaf7116e9527b99ce68abde2e9611990c
shubham3565-pixel <shubham.agrawal@appwrk.com>
2021-08-28 12:22:37 +0530

    First commit

---
+++ src/components/ex.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/ex.css"
+
+
+
+export default function navbar() {
+    return (
+        <div className="nav">
+            I am a Good Boy.
+        </div>
+    )
+}
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function footer() {
+    return (
+        <div>
+            <h2 style={{ color:"red", fontSize:"16px"}}>this is my footer</h2>
+        </div>
+    )
+}
